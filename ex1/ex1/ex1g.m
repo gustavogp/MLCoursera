082f3fc0903d4b8ee3e8d22fd03e7fc142d17551
Gustavo Pinheiro <gustavo@gusfedoravm.gushome.net>
2015-08-20 01:13:59 +0000

    Initial commit

---
+++ ex1/ex1/ex1g.m
@@ -0,0 +1,38 @@
+%% Machine Learning Online Class - Exercise 1: Linear Regression
+
+%% Initialization
+clear ; close all; clc
+
+fprintf('starting now ...\n')
+data = load('ex1data1.txt');
+y = data(:,2);
+m = length(y); % number of training examples
+
+fprintf('Running Gradient Descent ...\n')
+
+X = [ones(m, 1), data(:,1)]; % Add a column of ones to x
+theta = zeros(2, 1); % initialize fitting parameters
+
+% Some gradient descent settings
+iterations = 3500;
+alpha = 0.01;
+
+% compute and display initial cost
+computeCost(X, y, theta)
+
+% run gradient descent
+theta = gradientDescent(X, y, theta, alpha, iterations);
+
+% print theta to screen
+fprintf('Theta found by gradient descent: ');
+fprintf('%f %f \n', theta(1), theta(2));
+
+% Predict values for population sizes of 35,000 and 70,000
+predict1 = [1, 3.5] *theta;
+fprintf('For population = 35,000, we predict a profit of %f\n',...
+    predict1*10000);
+predict2 = [1, 7] * theta;
+fprintf('For population = 70,000, we predict a profit of %f\n',...
+    predict2*10000);
+
+
