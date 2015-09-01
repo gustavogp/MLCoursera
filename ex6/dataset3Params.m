59a654fcf7e07e7e7a012daf8671b41b083b8319
Gustavo Pinheiro <gustavo@gusfedoravm.gushome.net>
2015-09-01 06:23:32 +0000

    completing ex6

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,11 +23,24 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+candidates = [0.01 0.03 0.1 0.3 1 3 10 30];
+results = [];
+error = [];
 
+for c = 1:8
+	C = candidates(c)
+	for s = 1:8
+		sigma = candidates(s)
+		model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+		predictions = svmPredict(model, Xval);
 
-
-
-
+	results = [results(:,:);C sigma];
+	error = [error(:,:); mean(double(predictions ~= yval))];
+	endfor
+endfor
+[mi, mini] = min(error);
+C = results(mini, 1);
+sigma = results(mini, 2);
 
 % =========================================================================
 
